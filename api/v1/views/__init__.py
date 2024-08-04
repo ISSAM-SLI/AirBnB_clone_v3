5eb56e6dd4b6bf2bbf24d2157a4551754840d212
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-08-04 14:59:19 -0700

    Task 3

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that sets up the blueprint for API views
+"""
+
+from flask import Blueprint
+
+app_views = Blueprint('app_view', __name__, url_prefix='/api/v1/')
+
+from api.v1.views.index import *

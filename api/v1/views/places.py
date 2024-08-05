c750d38fc43a0472b9d4d549f86b557b66f31430
Abstargo <james.iharo@gmail.com>
2024-08-05 23:42:20 +0100

    this is

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,7 @@ from flask import jsonify, make_response, request, abort
 from models import storage
 from models.place import Place
 from models.city import City
-"""from models.user import User"""
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', strict_slashes=False)

38ab3f3016700bab7df61d85e476de6140226e16
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2024-08-04 22:52:01 -0700

    Task 7

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,10 @@
 Module that sets up the blueprint for API views
 """
 
-from api.v1.views.states import *
-from api.v1.views.index import *
-from api.v1.views.cities import *
 from flask import Blueprint
 
 app_views = Blueprint('app_view', __name__, url_prefix='/api/v1/')
+
+from api.v1.views.states import *
+from api.v1.views.index import *
+from api.v1.views.cities import *
